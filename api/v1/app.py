33649a2ed3bf8637260575524f30446603580c38
Omar ACHKIR <you@example.com>
2024-12-20 15:44:06 +0000

    app.py

---
+++ api/v1/app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" Flask Application """
+from models import storage
+from api.v1.views import app_views
+from api.v1.views.extensions import limiter
+from os import getenv
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+from pathlib import Path
+env_path = Path('/CarBnR_v4/.env')  # Update the path accordingly
+load_dotenv(dotenv_path=env_path)
+
+app = Flask(__name__)
+limiter.init_app(app)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.config['JWT_SECRET_KEY'] = getenv('JWT_SECRET_KEY')
+jwt = JWTManager(app)
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'CarBnR Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    host = getenv('CARBNR_API_HOST')
+    port = getenv('CARBNR_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5001'
+    app.run(host=host, port=port, threaded=True)
