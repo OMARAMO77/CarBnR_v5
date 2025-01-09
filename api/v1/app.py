00d11bb85d2d08a9df5683f2252c000d8ec52cb2
Omar ACHKIR <omar0642624692@gmail.com>
2025-01-09 20:24:00 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from models import storage
 from api.v1.views import app_views
 from api.v1.views.extensions import limiter
+from datetime import timedelta
 from os import getenv
 from flask import Flask, render_template, make_response, jsonify, request
 from flask_cors import CORS
@@ -24,6 +25,7 @@ app.config['JWT_SECRET_KEY'] = getenv('JWT_SECRET_KEY')
 app.config["JWT_COOKIE_CSRF_PROTECT"] = True  # Ensures CSRF tokens are issued and validated
 app.config["JWT_TOKEN_LOCATION"] = ["cookies"]  # Use cookies for tokens
 app.config["JWT_ACCESS_CSRF_HEADER_NAME"] = "X-CSRF-TOKEN"  # CSRF header name
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)  # 30 days
 jwt = JWTManager(app)
 
 
