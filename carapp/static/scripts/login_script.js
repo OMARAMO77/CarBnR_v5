04caadf47cf7cc46243f63c8096bc4bbc37e32b2
Omar ACHKIR <omar0642624692@gmail.com>
2025-01-12 21:42:11 +0000

    login_script.js

--- carapp/static/scripts/login_script.js
+++ carapp/static/scripts/login_script.js
@@ -1,10 +1,3 @@
-function isValidEmail(email) {
-    // Define a regular expression for validating email
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    // Test the email against the regex
-    return emailRegex.test(email);
-}
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('LogintoAccountForm');
     const loginButton = document.getElementById('LoginButton');
