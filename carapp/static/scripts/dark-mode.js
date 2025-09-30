db7b7811125ee7379af84a2da8b50378e9358570
Omar ACHKIR <omar0642624692@gmail.com>
2025-09-30 17:27:46 +0000

    dark-mode.js

--- carapp/static/scripts/dark-mode.js
+++ carapp/static/scripts/dark-mode.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const darkModeToggle = document.getElementById('darkModeToggle');
     const darkModeIcon = darkModeToggle.querySelector('i');
-    
+
     // Check for saved dark mode preference
     if (localStorage.getItem('darkMode') === 'enabled') {
         document.body.classList.add('dark-mode');
@@ -12,11 +12,11 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         darkModeToggle.innerHTML = '<i class="fas fa-moon"></i> Dark Mode';
     }
-    
+
     // Toggle dark mode
     darkModeToggle.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
-        
+
         if (document.body.classList.contains('dark-mode')) {
             localStorage.setItem('darkMode', 'enabled');
             darkModeIcon.classList.remove('fa-moon');
